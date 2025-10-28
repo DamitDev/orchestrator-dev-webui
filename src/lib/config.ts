5f34eb8de88139b3f0118ae262a8ab48a2a315ad
Dénes Márk <legekka@gmail.com>
2025-10-28 17:42:07 +0100

    refactor: update API and WebSocket URLs to use secure connections
    - Changed API_URL to use HTTPS instead of HTTP for enhanced security.
    - Updated WEBSOCKET_URL to use WSS for secure WebSocket connections.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,8 @@
-// Shared configuration for the application
-
 //export const API_URL = 'http://172.16.240.6:8082'
 //export const API_URL = 'http://localhost:8082'
-export const API_URL = 'http://oapi.iris.work/uat'
+export const API_URL = `https://oapi.iris.work/uat`
+//export const API_URL = 'http://oapi.iris.work/uat'
 
 // WebSocket configuration
-export const WEBSOCKET_URL = 'ws://localhost:8082/ws?client_id=webui'
+export const WEBSOCKET_URL = `wss://oapi.iris.work/uat/ws?client_id=webui`
 
