963540596c9ca0cc17cd8f3335db9e565f76de6d
Dénes Márk <legekka@gmail.com>
2025-11-05 13:52:11 +0100

    Cleanup

--- src/lib/config.ts
+++
@@ -1,8 +0,0 @@
-//export const API_URL = 'http://172.16.240.6:8082'
-//export const API_URL = 'http://localhost:8082'
-export const API_URL = `https://oapi.iris.work/uat`
-//export const API_URL = 'http://oapi.iris.work/uat'
-
-// WebSocket configuration
-export const WEBSOCKET_URL = `wss://oapi.iris.work/uat/ws?client_id=webui`
-
